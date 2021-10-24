7cd739206cb729b28568ee379d00aca279d7fa6a
236517 Dera Paweł <236517@edu.p.lodz.pl>
2021-10-24 19:28:11 +0200

    Modified .gitignore. Added static functions. Code refactoring. Tests added.

--- Sudoku_Project/src/test/java/sudoku/SudokuBoardTest.java
+++ Sudoku_Project/src/test/java/sudoku/SudokuBoardTest.java
@@ -9,10 +9,82 @@ class SudokuBoardTest {
     public SudokuBoardTest() {
     }
 
-    // TODO: Make solver test by generating random sudoku board from api https://sugoku.herokuapp.com/board?difficulty=easy
-    // TODO: or fill random squares with zeros and then try to solve the game. The rest test are getter, setter, and print board
+    @Test
+    void solveGameTest() {
+
+        SudokuBoard board = new SudokuBoard();
+        board.solveGame();
+
+        int[][] startingBoard = StaticFunctions.copyBoard(board);
+
+        StaticFunctions.clearRandomSquaresFromBoard(board);
+
+        board.solveGame();
+        int[][] afterSolvingBoard = StaticFunctions.copyBoard(board);
 
 
+        assertTrue(Arrays.deepEquals(startingBoard, afterSolvingBoard));
 
+    }
+
+    @Test
+    void getPositionNegativeTest() {
+
+        SudokuBoard board = new SudokuBoard();
+        board.solveGame();
+
+        assertEquals(-1, board.get(-1,1));
+        assertEquals(-1, board.get(0,-1));
+        assertEquals(-1, board.get(9,1));
+        assertEquals(-1, board.get(1,9));
+
+    }
+
+    @Test
+    void getPositionTest() {
+
+        SudokuBoard board = new SudokuBoard();
+        board.solveGame();
+
+        assertTrue(board.get(0,0) >= 1 && board.get(0,0) <= 9);
+
+    }
+
+    @Test
+    void setPositionNegativeTest() {
+
+        SudokuBoard board = new SudokuBoard();
+        board.solveGame();
+
+        int testingNumber = board.get(0,0);
+
+        board.set(-1,0, 1);
+        assertEquals(testingNumber, board.get(0,0));
+
+        board.set(0,-1, 1);
+        assertEquals(testingNumber, board.get(0,0));
+
+        board.set(9,0, 1);
+        assertEquals(testingNumber, board.get(0,0));
+
+        board.set(0,9, 1);
+        assertEquals(testingNumber, board.get(0,0));
+
+        board.set(0,0, -1);
+        assertEquals(testingNumber, board.get(0,0));
+
+        board.set(0,0, 10);
+        assertEquals(testingNumber, board.get(0,0));
+    }
+
+    @Test
+    void setPositionTest() {
+
+        SudokuBoard board = new SudokuBoard();
+
+        board.set(0,0, 1);
+        assertEquals(board.get(0,0), 1);
+
+    }
 
 }
